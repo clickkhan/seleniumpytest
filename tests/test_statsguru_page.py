32c9ee7e6ca548021edeebd6cd8ba56108abed1b
clickirfan@hotmail.com <clickirfan@hotmail.com>
2020-04-20 21:44:02 +0200

    Commit

--- tests/test_statsguru_page.py
+++ tests/test_statsguru_page.py
@@ -1,7 +1,7 @@
 from pageObjects.statsguru_page import StatsGuruPage
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import pytest
+from utilities.Base import BaseClass
 
 @pytest.fixture(scope = "class")
 #@pytest.fixture(scope = "function")
@@ -17,17 +17,30 @@ def setup(request):
     driver.close()
 
 @pytest.mark.usefixtures("setup")
-class TestStatsGuruPage:
-
-    # def test_confirm_cookie(self):
-    #     statsguru = StatsGuruPage(self.driver)
-    #     cookie_banner = statsguru.cookie_visible()
-    #     assert bool(cookie_banner) == True
-        #statsguru.confirm_cookie().click()
-        #assert bool(cookie_confirmed) == True
+class TestStatsGuruPage(BaseClass):
 
     def test_player_input(self):
         statsguru = StatsGuruPage(self.driver)
-        text_input = statsguru.input_player_name()
+        #self.driver.implicitly_wait(15)
+        text_input = statsguru.get_text_box()
         text_input.click()
+        text_input.clear()
+        assert text_input.get_attribute("value") == "", "Text Box was not empty."
         text_input.send_keys("Younis Khan")
+        statsguru.get_search_btn().click()
+        statsguru.confirm_cookie()
+        #statsguru.confirm_alert()
+        statsguru.get_player_btn().click()
+        statsguru.get_stats_links().click()
+
+    def test_stats_query(self):
+        statsguru = StatsGuruPage(self.driver)
+        self.select_dropdown_element(self.driver, statsguru.opposition_team_dropdown, "India").click()
+        statsguru.select_venue().click()
+        #venue = self.select_checkbox(self.driver, statsguru.venue_label, "away (home of opposition)")
+        #venue.click()
+        self.select_dropdown_element(self.driver, statsguru.ground_dropdown, "all grounds").click()
+        self.select_dropdown_element(self.driver, statsguru.host_country_dropdown, "India").click()
+        self.select_dropdown_element(self.driver, statsguru.seasons_dropdown, "all seasons").click()
+        self.select_radiobtn(self.driver, statsguru.view_format_label, "Batting formats").click()
+        statsguru.submit_query().click()
