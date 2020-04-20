ea05ee8db87f622e72a4c910f82220abb26c984c
clickirfan@hotmail.com <clickirfan@hotmail.com>
2020-04-20 21:51:28 +0200

    A new commit

--- pageObjects/statsguru_page.py
+++ pageObjects/statsguru_page.py
@@ -1,23 +1,61 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 class StatsGuruPage:
 
     cookie_confirm_btn = (By.CSS_SELECTOR, "button.cookie-continue")
-    player_name_input = (By.CSS_SELECTOR, "input.guruInput")
+    input_text_box = (By.CSS_SELECTOR, 'input[class="guruInput"]')
+    search_btn = (By.CSS_SELECTOR, 'input[class="guruButton"]')
+    player_btn = (By.CSS_SELECTOR, 'li[id="player"]')
+    stats_links = (By.CSS_SELECTOR, 'a[class="statsLinks"]')
+    opposition_team_dropdown = (By.CSS_SELECTOR, 'select[name="opposition"]')
+    venue_label = (By.CSS_SELECTOR, 'label[class="guruCheckboxLabel"]')
+    host_country_dropdown = (By.CSS_SELECTOR, 'select[name="host"]')
+    ground_dropdown = (By.CSS_SELECTOR, 'select[name="ground"]')
+    seasons_dropdown = (By.CSS_SELECTOR, 'select[name="season"]')
+    view_format_label = (By.CSS_SELECTOR, 'label[class="guruRadioLabel"]')
+    submit_btn = (By.CSS_SELECTOR, 'input[value="Submit query"]')
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(self.driver, 15)
+        self.wait = WebDriverWait(self.driver, 20)
 
-    def cookie_visible(self):
+    def get_cookie_visibility(self):
         return self.wait.until(EC.element_to_be_clickable(StatsGuruPage.cookie_confirm_btn))
 
     def confirm_cookie(self):
-        return self.wait.until(EC.presence_of_element_located(StatsGuruPage.cookie_confirm_btn))
+        self.wait.until(EC.presence_of_element_located(StatsGuruPage.cookie_confirm_btn)).click()
 
+    def confirm_alert(self):
+        self.wait.until(EC.alert_is_present())
+        alert = self.driver.switch_to.alert
+        alert.accept()
 
-    def input_player_name(self):
-        return self.wait.until(EC.presence_of_element_located(StatsGuruPage.player_name_input))
-        #return self.driver.find_element(*StatsGuruPage.player_name_input)
+    def get_text_box(self):
+        return self.wait.until(EC.presence_of_element_located(StatsGuruPage.input_text_box))
+        #return self.driver.find_element(*StatsGuruPage.input_text_box)
+
+    def get_search_btn(self):
+        return self.driver.find_element(*StatsGuruPage.search_btn)
+
+    def get_player_btn(self):
+        return self.driver.find_element(*StatsGuruPage.player_btn)
+
+    def get_stats_links(self):
+        #links = self.driver.find_elements(*StatsGuruPage.stats_links)
+        for link in self.driver.find_elements(*StatsGuruPage.stats_links):
+            if link.text == "Test matches player":
+                return link
+
+    def select_venue(self):
+        #venues = self.wait.until(EC.presence_of_all_elements_located(StatsGuruPage.venue_check_box))
+        venues = self.driver.find_elements(*StatsGuruPage.venue_label)
+        for venue in venues:
+            print(venue.text)
+            if venue.text == "away (home of opposition)":
+                return venue
+
+    def submit_query(self):
+        return self.driver.find_element(*StatsGuruPage.submit_btn)
--- utilities/Base.py
+++ utilities/Base.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.action_chains import ActionChains
 import pytest
 from pageObjects.home_page import HomePage
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
@@ -9,3 +12,25 @@ class BaseClass:
         #action = ActionChains(self.driver)
         action = ActionChains(self.driver)
         action.move_to_element(element).perform()
+
+    def select_dropdown_element(self, driver, element, text):
+        wait = WebDriverWait(self.driver, 15)
+        host_dropdown = Select(wait.until(EC.presence_of_element_located(element)))
+        for option in host_dropdown.options:
+            if option.text == text:
+                return option
+
+    def select_checkbox(self, driver, element, text):
+        #venues = self.wait.until(EC.presence_of_all_elements_located(*element))
+        venues = self.driver.find_elements(*element)
+        for venue in venues:
+            print(venue.text)
+            if venue.text == "text":
+                return venue
+
+    def select_radiobtn(self, driver, element, text):
+        view_formats = self.driver.find_elements(*element)
+        for format in view_formats:
+            print(format.text)
+            if format.text == text:
+                return format
